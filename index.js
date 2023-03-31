d25f318664fe3196c44bfc3fd6d7309bc7b49a3d
jim-low <robil12380@gmail.com>
2023-03-31 22:30:03 +0800

    feat: add training and basic chatbot code

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
 // link for reference: https://stormotion.io/blog/how-to-make-a-chatbot-using-js/#:~:text=In%20order%20to%20implement%20a,good%20choice%20for%20this%20task.
 
+    // Let's start with importing `NlpManager` from `node-nlp`. This will be responsible for training, saving, loading and processing.
+import { NlpManager } from "node-nlp";
+console.log("Starting Chatbot ...");
+
+// Creating new Instance of NlpManager class.
+const manager = new NlpManager({ languages: ["en"] });
+
+// Loading our saved model
+manager.load();
+
+// Loading a module readline, this will be able to take input from the terminal.
+import { createInterface } from "readline";
+var rl = createInterface(process.stdin, process.stdout);
+console.log("Chatbot started!");
+rl.setPrompt("> ");
+rl.prompt();
+rl.on("line", async function (line) {
+    // Here Passing our input text to the manager to get response and display response answer.
+    const response = await manager.process("en", line);
+    console.log(response.answer);
+    rl.prompt();
+}).on("close", function () {
+    process.exit(0);
+});
