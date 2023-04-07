1b55563914f7fb62233d6b17245d9dbfb1887958
jim-low <robil12380@gmail.com>
2023-04-08 00:48:34 +0800

    feat: add settings and training model for node-nlp v4

--- src/train.js
+++ src/train.js
@@ -1,24 +1,61 @@
-import { NlpManager } from "node-nlp";
+import { dockStart } from '@nlpjs/basic';
+// import { NlpManager } from "node-nlp";
 import { readdirSync, readFileSync } from "fs";
 
-const manager = new NlpManager({ languages: ["en"] });
-const files = readdirSync("./intents"); // read intent files in intents folder
 
 // loop through files, parse the string to object, pass to manager instance to train and process
-for (const file of files) {
-    let data = readFileSync(`./intents/${file}`);
-    data = JSON.parse(data);
-    const intent = file.replace(".json", "");
-    for (const question of data.questions) {
-        manager.addDocument("en", question, intent);
-    }
-    for (const answer of data.answers) {
-        manager.addAnswer("en", intent, answer);
-    }
+// this is for version 3
+// for (const file of files) {
+//     let data = readFileSync(`./intents/${file}`);
+//     data = JSON.parse(data);
+//     const intent = file.replace(".json", "");
+
+//     for (const question of data.questions) {
+//         manager.addDocument("en", question, intent);
+//     }
+
+//     for (const answer of data.answers) {
+//         manager.addAnswer("en", intent, answer);
+//     }
+// }
+
+// this is for version 4
+
+async function train_save(manager) { // train and save manager instance
+    await manager.train();
+    const minified = true;
+    const data = manager.export(minified);
+    manager.save()
 }
 
-async function train_save(){ // train and save manager instance
-    await manager.train();
-    manager.save();
-}
-train_save();
+(async () => {
+    const dock = await dockStart({
+        settings: {
+            nlp: {
+                forceNER: true,
+                languages: ['en']
+            },
+        },
+        use: ['Basic', 'LangEn']
+    });
+    const manager = dock.get('nlp');
+    const files = readdirSync("./intents"); // read intent files in intents folder
+
+    for (const file of files) {
+        let data = readFileSync(`./intents/${file}`)
+        data = JSON.parse(data)
+        const intent = file.replace(".json", "")
+
+        for (const utterance of data.utterances) {
+            manager.addDocument('en', utterance, intent);
+        }
+
+        for (const answer of data.answers) {
+            manager.addAnswer('en', intent, answer)
+        }
+
+        // TODO: add word conditions to extract context
+    }
+
+    train_save(manager);
+})();
