3dca352c986631905732459d6b70e79bc6d972a3
jim-low <robil12380@gmail.com>
2023-04-18 11:34:41 +0800

    feat: add more phrases to 'how do you do' and add 'what can you do'

--- features/small_talk.js
+++ features/small_talk.js
@@ -28,8 +28,29 @@ module.exports = function(controller) {
         await bot.reply(message, "Fuck you. How can I help?")
     })
 
-    controller.hears('how do you do', 'message', async (bot, message) => {
+    controller.hears(['how do you do', 'how was your day', 'how was your week', 'how are you'], 'message', async (bot, message) => {
         await bot.reply(message, 'I am doing great! I went to Sawcon last week. How can I help you?')
     })
 
+    controller.hears('what can you do', 'message', async (bot, message) => {
+        await bot.reply(message, `I am a chatbot titled ${context.botName} with the insufferable personality of Brian from RST2 Group 4.`)
+        await bot.reply(message, 'I am able to have a painful conversation with you as well as recommend and provide you with information on certain products.');
+        await bot.reply(message, {
+            text: "I am also well equipped with many fun facts! Do have a hear!",
+            quick_replies: [
+                {
+                    title: "Fun Fact",
+                    payload: "Give me a fun fact!"
+                },
+                {
+                    title: "Products",
+                    payload: "Show me your products!"
+                },
+                {
+                    title: "Cancel",
+                    payload: "Let me explore around a bit"
+                }
+            ]
+        });
+    })
 }
