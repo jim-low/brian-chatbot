0a046c58112078718ebf64cb0a6362e768391fba
jim-low <robil12380@gmail.com>
2023-04-30 22:00:58 +0800

    fix: add appropriate message

--- features/rickroll.js
+++ features/rickroll.js
@@ -1,10 +1,17 @@
 module.exports = function(controller) {
     controller.hears("rick roll", 'message', async (bot, message) => {
-        await bot.reply(message, 'congratulations, you have been rick rolled');
+        await bot.reply(message, 'congratulations, you have decided to get rick rolled');
         await bot.reply(message,
             `<video width="320" height="240" autoplay>
-        <source src="./rickroll.mp4" type="video/mp4">
-      Your browser does not allow your rick roll.
-      </video>`);
+            <source src="./rickroll.mp4" type="video/mp4">
+            Your browser does not allow your rick roll.
+            </video>`);
+        setInterval(async () => {
+            await bot.reply(message,
+                `<video width="320" height="240" autoplay>
+                <source src="./rickroll.mp4" type="video/mp4">
+                Your browser does not allow your rick roll.
+                </video>`);
+        }, 21300);
     })
 }   
