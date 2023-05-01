893d5187434213079c8665f7c3615358e2f7f7c3
jim-low <robil12380@gmail.com>
2023-05-01 17:42:19 +0800

    feat: add all possible intents

--- bot/botkit/classifications.js
+++ bot/botkit/classifications.js
@@ -3,9 +3,9 @@ const classifications = {
         hello: "small_talk.hello",
         name: "small_talk.name",
         niceToMeetYou: "small_talk.niceToMeetYou",
+        sawcon: "small_talk.sawcon",
         whatDoYouDo: "small_talk.whatDoYouDo",
         interests: "small_talk.interests",
-        sawcon: "small_talk.sawcon",
     },
     dumb_talk: {
         communism: "dumb_talk.communism",
@@ -29,7 +29,26 @@ const classifications = {
         askQuestion: "joke.askQuestion",
         noU: "joke.noU",
         meToo: "joke.meToo",
+        rickroll: "joke.rickroll",
+    },
+    inquiries: {
+        warranty: "inquiries.warranty",
+        warrantyRegistration: "inquiries.warrantyRegistration",
+        warrantyExtension: "inquiries.warrantyExtension",
+        return: "inquiries.return",
+        "100satisfaction": "inquiries.100satisfaction",
+        tac: "inquiries.tac",
+        repair: "inquiries.repair",
+        workingHours: "inquiries.workingHours",
+        troubleshoot: "inquiries.troubleshoot",
+        changeProduct: "inquiries.changeProduct",
+        findProduct: "inquiries.findProduct",
+        knowMore: "inquiries.knowMore",
+        installation: "inquiries.installation",
+        installation: "inquiries.installation",
+        cancel: "inquiries.cancel",
     }
 }
+
 
 module.exports = classifications;
